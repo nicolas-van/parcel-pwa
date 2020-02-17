431d84c229321f8249442703efb104518889c367
Nicolas Vanhoren <nicolas.vanhoren@gmail.com>
2020-02-17 16:05:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
 const elem = document.createElement('p')
-elem.innerText = 'Yeah, this website is working! You can install it and test to see it works offline too.'
+elem.innerText = 'Yeah, this website is working! You can install it and test to see it works offline too. Testeuh'
 
 document.querySelector('body').append(elem)
